d2c0ee20ad02111e35555a0c7754d0cf5cd238b5
Arif Mehniyar <arif.mehniyar@gmail.com>
2021-03-19 20:15:06 +0300

    fix icon svg

--- For-Vue-CLI/pagination/item-pagination-direction.vue
+++ For-Vue-CLI/pagination/item-pagination-direction.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="pg-item-pagination-direction" @click="enterPagItem" >
-      <vs-icon :icon="'chevron_'+direction" size="medium"></vs-icon>
+      <svg style="padding-top:5px;" v-if="direction == 'left'" viewBox="0 0 256 512" class="angle-left" :width="height" :height="height"><path class="angle-left" :fill="color" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
+      <svg  v-else  style="padding-top:5px;" viewBox="0 0 256 512" class="angle-right" :width="height" :height="height"><path class="angle-right" :fill="color" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
+     
   </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            height:"24",
+            color:"#000"
+        }
+    },
     props:{
         direction:{
             default:'left',
             type:[String]
-        }
+        },
+        
     },
     methods:{
         enterPagItem(e){
